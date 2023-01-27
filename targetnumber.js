1765ac4bb512640bbf33104e3b9f62fb57052e61
gireesh_hiremath <gireesh.hiremath@altorumleren.com>
2023-01-27 11:55:28 +0530

    targetnumber

---
+++ targetnumber.js
@@ -0,0 +1,12 @@
+function toSum(nums, target){
+    let map = new Map();
+    for(let i=0; i<nums.length; i++){
+        let complement = target -nums[i];
+        if(map.has(complement)){
+             return [map.get(complement), i];
+
+map.set(nums[i],i);            }
+    }
+    return[];
+}
+ console.log(toSum([2,7,11, 15], 9));
